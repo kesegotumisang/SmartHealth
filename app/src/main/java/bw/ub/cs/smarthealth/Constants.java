50a6f22bb9e7e602e67fb5d3bbd79c6ccbe310aa
kesegotumisang <201101433@ub.ac.bw>
2018-04-22 21:47:35 +0200

    Update 2

---
+++ app/src/main/java/bw/ub/cs/smarthealth/Constants.java
@@ -0,0 +1,5 @@
+package bw.ub.cs.smarthealth;
+
+public class Constants {
+    public static final String CHATS =" chats" ;
+}
--- app/src/main/java/bw/ub/cs/smarthealth/MainActivity.java
+++ app/src/main/java/bw/ub/cs/smarthealth/MainActivity.java
@@ -42,30 +42,11 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
-
     //Firebase Authentication
     public FirebaseAuth auth;
     public static FirebaseDatabase firebaseDatabase;
     public static DatabaseReference reference1;
     public static DatabaseReference reference2;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,25 +61,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
 
 
@@ -120,174 +83,14 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            auth.signOut();
-            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        if (id == R.id.action_search) {
+            //auth.signOut();
+            //startActivity(new Intent(MainActivity.this,LoginActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
 
-        ListView usersList;
-        TextView noUsersText;
-        ArrayList<String> al = new ArrayList<>();
-        int totalUsers = 0;
-        ProgressDialog pd;
-
-        LinearLayout layout;
-        RelativeLayout layout_2;
-        ImageView sendButton;
-        EditText messageArea;
-        ScrollView scrollView;
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-            firebaseDatabase = FirebaseDatabase.getInstance();
-            //Get the Current User's Chat
-            reference1 = firebaseDatabase.getReference().child("messages/"+ UserDetails.chatWith+ "_"+ UserDetails.username);
-            //Get the Practitioner's Chat
-            reference2 = firebaseDatabase.getReference().child("messages/"+ UserDetails.username+ "_"+ UserDetails.chatWith);
-            layout = rootView.findViewById(R.id.layout1);
-            layout_2 = rootView.findViewById(R.id.layout2);
-            sendButton = rootView.findViewById(R.id.sendButton);
-            messageArea = rootView.findViewById(R.id.messageArea);
-            scrollView = rootView.findViewById(R.id.scrollView);
-
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String messageText = messageArea.getText().toString();
-
-                if(!messageText.equals("")){
-                    Map<String, String> map = new HashMap<String, String>();
-                    map.put("message", messageText);
-                    map.put("user", UserDetails.username);
-                    reference1.push().setValue(map);
-                    reference2.push().setValue(map);
-                    messageArea.setText("");
-                }
-            }
-        });
-
-            /*reference1.addChildEventListener(new ChildEventListener() {
-                @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
-                    //GenericTypeIndicator genericTypeIndicator = dataSnapshot.
-                    Map map = dataSnapshot.getValue(Map.class);
-                    String message = map.get("message").toString();
-                    String userName = map.get("user").toString();
-
-                    if(userName.equals(UserDetails.username)){
-                        addMessageBox("You:-\n" + message, 1);
-                    }
-                    else{
-                        addMessageBox(UserDetails.chatWith + ":-\n" + message, 2);
-                    }
-                }
-
-                @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-
-            });
-
-*/
-
-
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-        public void addMessageBox(String message, int type){
-            TextView textView = new TextView(getContext());
-            textView.setText(message);
-
-            LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            lp2.weight = 1.0f;
-
-            if(type == 1) {
-                lp2.gravity = Gravity.LEFT;
-                textView.setBackgroundResource(R.drawable.bubble_in);
-            }
-            else{
-                lp2.gravity = Gravity.RIGHT;
-                textView.setBackgroundResource(R.drawable.bubble_out);
-            }
-            textView.setLayoutParams(lp2);
-            layout.addView(textView);
-            scrollView.fullScroll(View.FOCUS_DOWN);
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-    }
 }
