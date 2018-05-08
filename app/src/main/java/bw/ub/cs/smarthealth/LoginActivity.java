1729f33f36ac166d0d507b1de690c40486c4a268
kesegotumisang <201101433@ub.ac.bw>
2018-05-07 19:36:55 -0700

    Update 2

--- app/src/main/java/bw/ub/cs/smarthealth/LoginActivity.java
+++ app/src/main/java/bw/ub/cs/smarthealth/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
@@ -59,8 +60,8 @@ public class LoginActivity extends AppCompatActivity{
         private Button btnSignup, btnLogin, btnReset;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
+    private TextInputEditText mEmailView;
+    private TextInputEditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
     private TextView anonymousTextView;
@@ -72,6 +73,8 @@ public class LoginActivity extends AppCompatActivity{
     private DatabaseReference mUsersDBref;
     public static String LOGIN_ANONYMOUS_TAG = "anonymous";
     public String loginTag;
+    String email;
+    String password;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -79,18 +82,84 @@ public class LoginActivity extends AppCompatActivity{
             setContentView(R.layout.activity_login);
             //Get Firebase auth instance
             auth = FirebaseAuth.getInstance();
-            auth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    Intent inte = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(inte);
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
 
+            mEmailView = findViewById(R.id.emailEdt);
+            mPasswordView = findViewById(R.id.passwordEdt);
+            anonymousTextView = findViewById(R.id.txt_anonymous);
+            btnLogin = findViewById(R.id.email_sign_in_button);
+
+           // final String email = String.valueOf(mEmailView.getText());
+            //final String password = String.valueOf(mPasswordView.getText());
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = mEmailView.getText().toString();
+                password = mPasswordView.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+               // progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                              //  progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+
+            anonymousTextView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    auth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Intent inte = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(inte);
+
+                        }
+                    });
                 }
             });
 
 
 
+
+
+
     }
+
 
     private void createUserInDb(String userId, String displayName, String email){
         mUsersDBref = FirebaseDatabase.getInstance().getReference().child("Users");
